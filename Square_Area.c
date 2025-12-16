dab965988acde8f03378baf1d562accbb2f54211
Pranxvv-v <pranxvvkaushik@gmail.com>
2025-12-16 21:01:08 +0530

    Added Square_Area.c

---
+++ Square_Area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter the length of the square: " );
+    int side;
+    scanf("%d", &side);
+
+    printf("Area of the square is: %d \n", side*side);
+    return 0;
+}
