223dd961a2c30520bc07250ec446b7f4ff7c3834
Pranxvv-v <pranxvvkaushik@gmail.com>
2025-12-16 20:50:32 +0530

    Added Add.c

---
+++ Add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Enter first number: ");
+    int a;
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    int b;
+    scanf("%d", &b);
+
+    printf("Sum: %d \n", a+b);
+    return 0;
+}
